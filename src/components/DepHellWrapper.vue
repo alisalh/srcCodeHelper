4c56f5eaa8fd0866fdaad0a38fbd65f0afd258eb
alisalh <508091602@qq.com>
2019-04-15 11:06:28 +0800

    feat: 改善文件结构图

--- src/components/DepHellWrapper.vue
+++ src/components/DepHellWrapper.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="dep-hell-wrapper bl-card-shadow" ref="root">
-    <svg ref="svg">
-      <!-- <g class="legend-wrapper" :style="{transform:`translateX(${(svgWidth-legendWrapperWidth)/2}px)`}">
-        <g :style="{transform:`translate(${idx*(legend.width+legendLabelLen+20)}px,20px)`}" v-for="(val,key,idx) in colorMap">
-          <rect :width="legend.width" :height="legend.height" :fill="val" rx="5" ry="5" @click="toggleType(idx)" :class="{'disabled-type':!typeStatus[idx]}"></rect>
-          <text :textLength="legendLabelLen" :x="legend.width+3" :y="legend.height/2" lengthAdjust="spacingAndGlyphs" alignment-baseline="central">{{key}}</text>
-        </g>
-      </g> -->
-    </svg>
   </div>
 </template>
 <script type="text/javascript">
@@ -19,20 +11,11 @@ export default {
       svg: null,
       svgWidth: null,
       svgHeight: 0,
-      legendLabelLen: 60,
-      legend: {
-        width: 50,
-        height: 20,
-      },
-      legendInnerPadding: 3,
-      legendOuterPadding: 20,
-      lenTreshold: 0,
-      typeStatus: [true, true, true], //indicates whether corresponding type(dep link and rect style) is visible or not
-      depLinkGroupG: null,
-      longestDepLen: 99999,
+      lenTreshold: null,
       hierarchyHiehgt: 110,
       stackHeight: 30,
-      fileDepInfo: null // store dep info for each file
+      fileDepInfo: null, // store dep info for each file
+      centerSvg: null,
     }
   },
   props: ['root', 'colorMap'],
@@ -41,17 +24,9 @@ export default {
     console.log('root in dephell updated')
   },
   computed: {
-    dendrogramR() { return Math.min(this.svgWidth, this.svgHeight) / 2 - 130 },
-    legendWrapperWidth() {
-      let num = Object.keys(this.colorMap).length
-      return (this.legend.width + this.legendInnerPadding + this.legendLabelLen) * num + (this.legendOuterPadding) * (num - 1)
-    }
+    dendrogramR() { return Math.min(this.svgWidth, this.svgHeight) / 2 - 130 }
   },
   methods: {
-    toggleType(idx) {
-      // splice替换元素值
-      this.typeStatus.splice(idx, 1, !this.typeStatus[idx])
-    },
     drawHierachy() {
       let vm = this
       var formatNumber = d3.format(",d");
@@ -66,10 +41,11 @@ export default {
         .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
         .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
 
-      var svg = this.centerSvg.append("g")
+      var hierarchyG = this.svg.append('g')
+        .attr('transform', 'translate(' + this.svgWidth / 2 + ',' + (this.svgHeight / 2) + ')')
 
       //partition the tree and attach additional attr on root as well as its descendants
-      var node = svg.selectAll(".hierarchy-node").data(partition(this.root).descendants().slice(1)).enter().append("g")
+      var node = hierarchyG.selectAll(".hierarchy-node").data(partition(this.root).descendants().slice(1)).enter().append("g")
       node.append("path")
         .attr("class", "hierarchy-node")
         .attr('id', (d, i) => 'hierarchy-node-'+i)
@@ -83,24 +59,26 @@ export default {
           return '#e5d8bd' 
         })
         .each((d, i) => {
-          var firstArcSection = /(^.+?)L/
-          let curNode = node.select('#hierarchy-node-'+i)
-          var newArc = firstArcSection.exec(curNode.attr('d'))[1]
-          newArc = newArc.replace(/,/g, ' ')
-          if(arc.centroid(d)[1] > 0){
-            var startLoc = /M(.*?)A/,
-              middleLoc = /A(.*?)0 0 1/,
-              endLoc = /0 0 1 (.*?)$/
-            var newStart = endLoc.exec(newArc)[1],
-              newEnd = startLoc.exec(newArc)[1],
-              middleSec = middleLoc.exec(newArc)[1]
-            newArc = 'M' + newStart + 'A' + middleSec + '0 0 0' + newEnd 
-          }
-          node.append('path')
-            .attr('class', 'hiddenDonutArcs')
-            .attr('id', 'donutArc'+i)
-            .attr('d', newArc)
-            .style('fill', 'none')
+          if(d.data.type === 'dir'){
+            var firstArcSection = /(^.+?)L/
+            let curNode = node.select('#hierarchy-node-'+i)
+            var newArc = firstArcSection.exec(curNode.attr('d'))[1]
+            newArc = newArc.replace(/,/g, ' ')
+            if(arc.centroid(d)[1] > 0){
+              var startLoc = /M(.*?)A/,
+                middleLoc = /A(.*?)0 0 1/,
+                endLoc = /0 0 1 (.*?)$/
+              var newStart = endLoc.exec(newArc)[1],
+                newEnd = startLoc.exec(newArc)[1],
+                middleSec = middleLoc.exec(newArc)[1]
+              newArc = 'M' + newStart + 'A' + middleSec + '0 0 0' + newEnd 
+            }
+            node.append('path')
+              .attr('class', 'hiddenDonutArcs')
+              .attr('id', 'donutArc'+i)
+              .attr('d', newArc)
+              .style('fill', 'none')
+            }
         })
         .append("title")
         .text((d) => d.data.name)
@@ -133,89 +111,6 @@ export default {
             else return name
           }
         })
-    },
-    drawDendrogram() {
-      let vm = this
-      let svg=this.centerSvg.append("g")
-      var cluster = d3.cluster()
-        .size([360, this.dendrogramR]).separation(() => 1);
-      cluster(this.root)
-      return 
-           var link = svg.append("g").attr("class", "dendrogram-links").selectAll(".link")
-              .data(this.root.descendants().slice(1))
-              .enter().append("path")
-              .attr("class", "link")
-              .attr("d", function(d) {
-                return "M" + project(d.x, d.y) +
-                  "C" + project(d.x, (d.y + d.parent.y) / 2) +
-                  " " + project(d.parent.x, (d.y + d.parent.y) / 2) +
-                  " " + project(d.parent.x, d.parent.y);
-              });
-
-            var node = svg.append("g").attr("class", "dendrogram-nodes").selectAll(".node")
-              .data(this.root.descendants())
-              .enter().append("g")
-              .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-              .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; });
-
-           node.append("circle")
-              .attr("r", 2.5)
-
-            node.append("title").text(d => d.data.name)
-            node.append("text")
-              .attr("dy", "0.31em")
-
-      function project(x, y) {
-        var angle = (x - 90) / 180 * Math.PI,
-          radius = y;
-        return [radius * Math.cos(angle), radius * Math.sin(angle)];
-      }
-    },
-    drawDepLinks() {
-      let data = this.badDeps.slice()
-      let longGroup = data.find(d => d.type === 'long'),
-        svg = this.centerSvg.append("g")
-        .attr("class", "dep-link-wrapper"),
-        vm = this
-      let depLink = svg.append("g").attr("class", "dep-links-group").selectAll(".dep-links-group")
-      var line = d3.radialLine()
-        .curve(d3.curveBundle.beta(0.85))
-        .radius(function(d) { return d.y; })
-        .angle(function(d) { return d.x / 180 * Math.PI; });
-
-      this.depLinkGroupG = depLink.data(data).enter().append("g")
-        .attr("class", d => `dep-links-group__${d.type}`)
-      this.depLinkGroupG.selectAll(".dep-link")
-        .data(d => extractShortesPath(d.paths))
-        .enter().append("path")
-        .each(function(d) { d.source = d[0], d.target = d[d.length - 1]; })
-        .attr("class", "dep-link")
-        .attr("d", line)
-      //extract shortest paht between each node in paths to achieve edge bundling
-      function extractShortesPath(paths) {
-        let nodes = vm.root.leaves()
-        var map = {},
-          shortestPathArr = [];
-        // Compute a map from name to node.
-        nodes.forEach(function(d) {
-          map[d.data.name] = d;
-        });
-
-        // For each path, construct a link from the source to target node.
-        paths.forEach(function({ path }) {
-          path.reduce(function(a, b) {
-              try{
-                shortestPathArr.push(map[a].path(map[b]))
-              }
-              catch(e){
-                console.log(path)
-              }
-              // shortestPathArr.push(map[a].path(map[b]))
-            return b
-          })
-        })
-        return shortestPathArr
-      }
     },
     drawRadialStack() {
       //get StackData from badDeps
@@ -257,39 +152,37 @@ export default {
         })
       seiresG.selectAll('g').data(d => d).enter().append('path').attr('d', arc)
 
-    },
-    initSvg() {
-      this.svg = d3.select(".dep-hell-wrapper svg")
-      this.centerSvg = d3.select(".dep-hell-wrapper svg .center-g")
     }
   },
   watch: {
-    //show whether particular dep-link-group is visible according to status
-    typeStatus(status) {
-      this.depLinkGroupG.attr('stroke-opacity', (d, idx) => {
-        if (status[idx])
-          return 0.3
-        else
-          return 0
-      })
+    lenTreshold(val){
+      if(val)
+        this.$axios.get('files/getStackData', {
+          threshold: val
+        }).then(({ data }) => {
+          
+        })
     },
-    // badDeps(val) {
-    //   if (val) {
-    //     this.initSvg()
-    //     // this.drawDendrogram()
-    //     this.drawHierachy()
-    //     // this.drawRadialStack()
-    //     // this.drawDepLinks()
-    //   }
-    // }
+  },
+  created(){
+    const requiredData = ['root']
+    let cnt = 0
+    requiredData.forEach(d => {
+      this.$watch(d, val => {
+        if(val) cnt++
+        if(cnt === requiredData.length) {
+          this.drawHierachy()
+        }
+      })
+    })
   },
   mounted() {
     this.svgWidth = Math.floor(this.$refs.root.clientWidth)
     this.svgHeight = Math.floor(this.$refs.root.clientHeight)
-    // Todo:目前需要手动减10
-    d3.select(".dep-hell-wrapper svg").attr("width", this.svgWidth).attr("height", this.svgHeight)
-      .append("g").attr("class", "center-g")
-      .attr("transform", "translate(" + this.svgWidth / 2 + "," + (this.svgHeight / 2) + ")")
+    this.svg = d3.select(this.$refs.root).append('svg')
+      .attr('width', this.svgWidth)
+      .attr('height', this.svgHeight)
+    
     this.$bus.$on('threshold-selected', d =>{
       this.lenTreshold = d
       // this.drawRadialStack()
@@ -300,50 +193,10 @@ export default {
 </script>
 <style type="text/css" lang="scss">
 .dep-hell-wrapper {
-  .legend-wrapper {
-    rect.disabled-type {
-      fill: grey;
-    }
-  }
+  height: 100%;
   .hierarchy-node {
     &:hover {
       transform: translate(20);
-    }
-  }
-  .dendrogram-links {
-    path {
-      stroke: steelblue;
-      stroke-opacity: 1.0;
-      fill: none;
-      pointer-events: none;
-    }
-  }
-  .dendrogram-nodes {
-    opacity: 1.0;
-  }
-  /*   .dep-link {
-    stroke: red;
-    stroke-opacity: 0.4;
-    fill: none;
-    pointer-events: none;
-  } */
-  .dep-links-group {
-    stroke-width: 4px;
-    fill: none; 
-    .dep-links-group__indirect {
-      path {
-        stroke: #66c2a5;
-      }
-    }
-    .dep-links-group__direct {
-      path {
-        stroke: #377eb8;
-      }
-    }
-    .dep-links-group__long {
-      path {
-        stroke: #d53e4f;
-      }
     }
   }
 }
